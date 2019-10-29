435043eaedc1c93c34e484da92a93ab301f32175
chenpes@hotmail.com <chenpes@hotmail.com>
2019-10-29 00:28:21 -0300

    API Usuario

---
+++ Chenpess.QuickBuy.Web/Controllers/UsuarioController.cs
@@ -0,0 +1,49 @@
+﻿using Chenpess.QuickBuy.Dominio.Contratos;
+using Chenpess.QuickBuy.Dominio.Entidades;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chenpess.QuickBuy.Web.Controllers
+{
+    [Route("api/[controller]")]
+    public class UsuarioController : Controller
+    {
+        private readonly IUsuarioRepositorio _UsuarioRepositorio;
+        public UsuarioController(IUsuarioRepositorio UsuarioRepositorio)
+        {
+            _UsuarioRepositorio = UsuarioRepositorio;
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody]Usuario Usuario)
+        {
+            try
+            {
+                _UsuarioRepositorio.adicionar(Usuario);
+                return Created("api/Usuario", Usuario);
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.ToString());
+            }
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                return Ok(_UsuarioRepositorio.obterTodos());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.ToString());
+            }
+        }
+
+    }
+
+}

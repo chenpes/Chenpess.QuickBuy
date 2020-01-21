2ee31d99126e074494aaaef19b5db0f7e63e38b0
chenpes@hotmail.com <chenpes@hotmail.com>
2020-01-21 17:00:13 -0300

    Inserção da tela de login no sistema

--- Chenpess.QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
+++ Chenpess.QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
@@ -6,5 +6,20 @@ import { Component } from "@angular/core";
     styleUrls: ["./login.component.css"]
 })
 export class LoginComponent {
+    
+    public titulo = "Quick Buy - Faça suas compras!";
+    public email = "";
+    public senha = "";
+
+    entrar() {
+        if (this.email != "chenpess@gmail.com" || this.senha != "12345") {
+            alert('Usuario não cadastrado');
+        }
+        else {
+            alert('Seja bem vindo Claudio');
+        }
+
+          
+    }
 
 }

ae207225d2b171171ba0277e60c30d851f2fe247
chenpes@hotmail.com <chenpes@hotmail.com>
2020-01-23 13:43:52 -0300

    Inserção do modelo de usuario em angular

--- Chenpess.QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
+++ Chenpess.QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { Usuario } from "../../modelo/usuario";
 
 @Component({
     selector: "app-login",
@@ -6,17 +7,22 @@ import { Component } from "@angular/core";
     styleUrls: ["./login.component.css"]
 })
 export class LoginComponent {
+    public usuario; 
     
     public titulo = "Quick Buy - Faça suas compras!";
-    public email = "";
-    public senha = "";
+    public usuarioautenticado: boolean;
+
+    constructor() {
+        this.usuario = new Usuario();
+    }
 
     entrar() {
-        if (this.email != "chenpess@gmail.com" || this.senha != "12345") {
+        if (this.usuario.email != "chenpess@gmail.com" || this.usuario.senha != "12345") {
             alert('Usuario não cadastrado');
         }
         else {
-            alert('Seja bem vindo Claudio');
+            this.usuarioautenticado = true;
+            alert('Seja bem vindo ' + this.usuario.email + " - " + this.usuario.senha);
         }
 
           
